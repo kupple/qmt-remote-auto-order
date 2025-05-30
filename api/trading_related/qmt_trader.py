7cd48e3293b9a74f36f69ea291bff5bd6c2e24fe
kupple <rtys788@icloud.com>
2025-05-30 15:39:11 +0800

    #

--- api/trading_related/qmt_trader.py
+++ api/trading_related/qmt_trader.py
@@ -191,33 +191,27 @@ class qmt_trader:
       pass
     return df
 
- def reverse_repurchase_of_treasury_bonds(self,security='131810.SZ',buy_ratio=0.00001, order_type=xtconstant.STOCK_SELL
+ def reverse_repurchase_of_treasury_bonds(self,security='131810.SZ', order_type=xtconstant.STOCK_SELL
                       ,price_type=xtconstant.FIX_PRICE,strategy_name='',order_remark=''):
-   '''
-   国债逆回购
-   '''
-   # 对交易回调进行订阅，订阅后可以收到交易主推，返回0表示订阅成功
-   account=self.balance()
-   av_cash=account['可用金额'].tolist()[-1]
-   av_cash=float(av_cash)
-   av_cash=av_cash*buy_ratio
-   spot_data=self.data.get_full_tick(code_list=[security])
-   price=spot_data[security]['lastPrice']
-   price=float(price)
-   amount=math.floor((av_cash/price)/10)*10
-   subscribe_result = self.xt_trader.subscribe(self.acc)
-   print(self.xt_trader.query_stock_asset_async(account=self.acc,callback=subscribe_result))
-   #print(subscribe_result)
-   stock_code =self.adjust_stock(stock=security)
-   #price=self.select_slippage(stock=security,price=price,trader_type='sell')
-   order_volume=amount
-   # 使用指定价下单，接口返回订单编号，后续可以用于撤单操作以及查询委托状态
-   if order_volume>0:
-     fix_result_order_id = self.xt_trader.order_stock(account=self.acc,stock_code=stock_code, order_type=order_type,
-                               order_volume=order_volume, price_type=price_type,
-                               price=price, strategy_name=strategy_name, order_remark=order_remark)
-     text='国债逆回购交易类型{} 代码{} 价格{} 数量{} 订单编号{}'.format(order_type,stock_code,price,order_volume,fix_result_order_id)
-     return True,text
-   else:
-     text='国债逆回购卖出 标的{} 价格{} 委托数量{}小于0有问题'.format(stock_code,price,order_volume)
-     return False,text
+  '''
+  国债逆回购
+  '''
+  # 对交易回调进行订阅，订阅后可以收到交易主推，返回0表示订阅成功
+  account=self.balance()
+  av_cash=account['可用金额'].tolist()[-1]
+  av_cash=float(av_cash)
+  spot_data=self.data.get_full_tick(code_list=[security])
+  price=spot_data[security]['lastPrice']
+  price=float(price)
+  stock_code =self.adjust_stock(stock=security)
+  order_volume=av_cash // 100
+  if order_volume>0:
+    fix_result_order_id = self.xt_trader.order_stock(account=self.acc,stock_code=stock_code, order_type=order_type,
+                order_volume=order_volume, price_type=price_type,
+                price=price, strategy_name=strategy_name, order_remark=order_remark)
+    text='国债逆回购交易类型{} 代码{} 价格{} 数量{} 订单编号{}'.format(order_type,stock_code,price,order_volume,fix_result_order_id)
+    return True,text
+  else:
+    text='国债逆回购卖出 标的{} 价格{} 委托数量{}小于0有问题'.format(stock_code,price,order_volume)
+    return False,text
+
